54bb6da931a840f2bdb3c19371a6123eb4e7e58f
Victor Hugo <victorhleme.dev@gmail.com>
2023-06-28 17:50:04 -0300

    Updated CV to be downloaded from Firebase Storage

--- src/components/portfolio/header/CTA.jsx
+++ src/components/portfolio/header/CTA.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import CV from '../../../assets/VictorHugoLemeCVpt.pdf'
-import EngCV from '../../../assets/VictorHugoLemeCVen.pdf'
 import { useTranslation } from 'react-i18next'
 import Translator from '../i18n/Translator'
 
 const CTA = () => {
   const { i18n } = useTranslation()
-  let cvVersion = i18n.language === 'pt-BR' ? CV : EngCV
+  const cvEng = "https://firebasestorage.googleapis.com/v0/b/portfolio-victorhleme.appspot.com/o/CV%2FVictorHugoLemeCVen.pdf?alt=media&token=f5e3c906-e19c-4edd-8d1a-08238468ecfb"
+  const cvPor = "https://firebasestorage.googleapis.com/v0/b/portfolio-victorhleme.appspot.com/o/CV%2FVictorHugoLemeCVpt.pdf?alt=media&token=959062cc-9426-440c-ae57-6dd59641061d"
+  let cvVersion = i18n.language === 'pt-BR' ? cvPor : cvEng
 
   return (
     <div className='cta'>
-      <a className="btn" href={cvVersion} download><Translator path='CTA.download' /></a>
+      <a className="btn" target="_blank" rel="noreferrer" href={cvVersion} download><Translator path='CTA.download' /></a>
       <a className="btn btn-primary" href='#contact'><Translator path='CTA.contact' /></a>
     </div>
   )
