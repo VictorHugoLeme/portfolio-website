32beaaae7d8dc545c64f2fa151b360caef9e92d0
Victor Hugo <victorhleme.dev@gmail.com>
2023-06-28 20:46:30 -0300

    Updating CV links

--- src/components/portfolio/header/CTA.jsx
+++ src/components/portfolio/header/CTA.jsx
@@ -4,8 +4,8 @@ import Translator from '../i18n/Translator'
 
 const CTA = () => {
   const { i18n } = useTranslation()
-  const cvEng = "https://firebasestorage.googleapis.com/v0/b/portfolio-victorhleme.appspot.com/o/CV%2FVictorHugoLemeCVen.pdf?alt=media&token=f5e3c906-e19c-4edd-8d1a-08238468ecfb"
-  const cvPor = "https://firebasestorage.googleapis.com/v0/b/portfolio-victorhleme.appspot.com/o/CV%2FVictorHugoLemeCVpt.pdf?alt=media&token=959062cc-9426-440c-ae57-6dd59641061d"
+  const cvEng = "https://firebasestorage.googleapis.com/v0/b/portfolio-victorhleme.appspot.com/o/CV%2FVictorHugoLemeCVen.pdf?alt=media&token=8e8cfd08-5511-41f7-9207-efe8e2ee9b95"
+  const cvPor = "https://firebasestorage.googleapis.com/v0/b/portfolio-victorhleme.appspot.com/o/CV%2FVictorHugoLemeCVpt.pdf?alt=media&token=72a4f032-96d2-4454-b9fa-9cb690fc14ca"
   let cvVersion = i18n.language === 'pt-BR' ? cvPor : cvEng
 
   return (
