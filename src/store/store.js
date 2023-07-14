da834d691ef243d34dd654c83851fed9d4d638e2
Victor Hugo <victorhleme.dev@gmail.com>
2023-07-14 16:13:05 -0300

    Update data to be stored in localStorage after being fetched

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,16 @@
 import api from "../client/api.js";
 
 const getData = async () => {
-  return api.get()
-    .then(res => {
+  return sessionStorage.getItem('data')
+    ? JSON.parse(sessionStorage.getItem('data'))
+    : api.get()
+      .then(res => {
         sessionStorage.setItem('data', JSON.stringify(res.data.data))
-      return res.data.data
-    })
-    .catch(err => {
-      console.log(err)
-    })
+        return res.data.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
 }
 
 export { getData }
