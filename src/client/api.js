50ee9179db645a0e5efbc7be0047076672d8ae7b
Victor Hugo <victorhleme.dev@gmail.com>
2023-10-30 16:27:37 -0300

    Update data fetch endpoint

--- src/client/api.js
+++ src/client/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://1otoj96rr9.execute-api.us-east-1.amazonaws.com/default/get-victor-h-leme-portfolio-data/8ce8f5c3-f384-45bf-a0eb-9878d0a1ffdc",
+  baseURL: "https://lqh2eehpfzqqhpfyvbzf2v4qfi0mfcnz.lambda-url.us-east-1.on.aws/8ce8f5c3-f384-45bf-a0eb-9878d0a1ffdc",
 });
 
 export default api;
